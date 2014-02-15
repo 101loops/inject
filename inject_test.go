238f77dc427d70cca6b5674909e96a99ba666980
stephanos <github@stephanbehnke.de>
2014-02-15 17:41:28 +0100

    draft: map functions and use their return type for injection

--- inject_test.go
+++ inject_test.go
@@ -1,9 +1,11 @@
-package inject_test
+package inject
 
 import (
-	"github.com/codegangsta/inject"
+	"fmt"
+	"time"
 	"reflect"
 	"testing"
+	"math/rand"
 )
 
 type SpecialString interface {
@@ -15,21 +17,25 @@ type TestStruct struct {
 	Dep3 string
 }
 
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
 /* Test Helpers */
 func expect(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
-		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+		t.Errorf("Expected: %v (type %v) - Got: %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
 }
 
 func refute(t *testing.T, a interface{}, b interface{}) {
 	if a == b {
-		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+		t.Errorf("Did not expect %v (type %v) - Got: %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
 }
 
 func Test_InjectorInvoke(t *testing.T) {
-	injector := inject.New()
+	injector := New()
 	expect(t, injector == nil, false)
 
 	dep := "some dependency"
@@ -46,7 +52,7 @@ func Test_InjectorInvoke(t *testing.T) {
 }
 
 func Test_InjectorInvokeReturnValues(t *testing.T) {
-	injector := inject.New()
+	injector := New()
 	expect(t, injector == nil, false)
 
 	dep := "some dependency"
@@ -60,12 +66,12 @@ func Test_InjectorInvokeReturnValues(t *testing.T) {
 		return "Hello world"
 	})
 
-	expect(t, result[0].String(), "Hello world")
 	expect(t, err, nil)
+	expect(t, result[0].String(), "Hello world")
 }
 
 func Test_InjectorApply(t *testing.T) {
-	injector := inject.New()
+	injector := New()
 
 	injector.Map("a dep").MapTo("another dep", (*SpecialString)(nil))
 
@@ -78,10 +84,10 @@ func Test_InjectorApply(t *testing.T) {
 }
 
 func Test_InterfaceOf(t *testing.T) {
-	iType := inject.InterfaceOf((*SpecialString)(nil))
+	iType := InterfaceOf((*SpecialString)(nil))
 	expect(t, iType.Kind(), reflect.Interface)
 
-	iType = inject.InterfaceOf((**SpecialString)(nil))
+	iType = InterfaceOf((**SpecialString)(nil))
 	expect(t, iType.Kind(), reflect.Interface)
 
 	// Expecting nil
@@ -89,11 +95,11 @@ func Test_InterfaceOf(t *testing.T) {
 		rec := recover()
 		refute(t, rec, nil)
 	}()
-	iType = inject.InterfaceOf((*testing.T)(nil))
+	iType = InterfaceOf((*testing.T)(nil))
 }
 
 func Test_InjectorGet(t *testing.T) {
-	injector := inject.New()
+	injector := New()
 
 	injector.Map("some dependency")
 
@@ -102,11 +108,112 @@ func Test_InjectorGet(t *testing.T) {
 }
 
 func Test_InjectorSetParent(t *testing.T) {
-	injector := inject.New()
+	injector := New()
 	injector.MapTo("another dep", (*SpecialString)(nil))
 
-	injector2 := inject.New()
+	injector2 := New()
 	injector2.SetParent(injector)
 
-	expect(t, injector2.Get(inject.InterfaceOf((*SpecialString)(nil))).IsValid(), true)
+	expect(t, injector2.Get(InterfaceOf((*SpecialString)(nil))).IsValid(), true)
+}
+
+func Test_InjectorInvokeFactory(t *testing.T) {
+	injector := New()
+
+	dep := "some dependency"
+	injector.Map(func() string {
+		return dep
+	})
+	dep2 := "another dep"
+	injector.MapTo(func() string {
+		return dep2
+	}, (*SpecialString)(nil))
+
+	res, err := injector.Invoke(func(d1 string, d2 SpecialString) string {
+		expect(t, d1, dep)
+		expect(t, d2, dep2)
+		return dep
+	})
+
+	expect(t, err, nil)
+	expect(t, res[0].String(), dep)
+}
+
+func Test_InjectorInvokeCascadingFactory(t *testing.T) {
+	injector := New()
+
+	answer := 42
+	injector.Map(func() int {
+		return answer
+	})
+	question := "What do you get if you multiply six by nine?"
+	injector.Map(func(answer int) string {
+		return fmt.Sprintf("%v %v", question, answer)
+	})
+
+	sentence := fmt.Sprintf("%v %v", question, answer)
+	res, err := injector.Invoke(func(d1 string) string {
+		expect(t, d1, sentence)
+		return sentence
+	})
+
+	expect(t, err, nil)
+	expect(t, res[0].String(), sentence)
+}
+
+func Test_InjectorInvokeDependencyLoop(t *testing.T) {
+	injector := New()
+
+	dep := "some dependency"
+	injector.Map(func(d2 string) string {
+		return dep
+	})
+
+	_, err := injector.Invoke(func(d string) {
+		t.Errorf("expected an error, not %v", d)
+	})
+
+	if err == nil {
+		t.Errorf("expected an error")
+	}
+}
+
+func Test_InjectorInvokeWithParentDependency(t *testing.T) {
+	injector := New()
+	dep := "some dependency"
+	injector.Map(func(d2 int) string {
+		return dep
+	})
+
+	injector2 := New()
+	injector2.Map(42)
+	injector2.SetParent(injector)
+
+	res, err := injector2.Invoke(func(d1 string) string {
+		expect(t, d1, dep)
+		return dep
+	})
+
+	expect(t, err, nil)
+	expect(t, res[0].String(), dep)
+}
+
+func Test_InjectorInvokeCaching(t *testing.T) {
+	injector := New()
+
+	injector.Map(func() int {
+		return rand.Intn(1000000)
+	})
+	injector.MapTo(func() string {
+		return "!"
+	}, (*SpecialString)(nil))
+	injector.Map(func(c SpecialString, n int) string {
+		return fmt.Sprintf("%v%v", n, c)
+	})
+
+	_, err := injector.Invoke(func(s string, c SpecialString, n int) {
+		expect(t, s, fmt.Sprintf("%v%v", n, c))
+	})
+
+	expect(t, err, nil)
 }
